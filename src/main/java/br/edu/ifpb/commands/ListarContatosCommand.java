ce46b2b176d894251c50d80bea9d84f9181a3a01
claradiass <claragoncalves370@gmail.com>
2023-11-08 11:11:16 -0300

    .

--- src/main/java/br/edu/ifpb/commands/ListarContatosCommand.java
+++ src/main/java/br/edu/ifpb/commands/ListarContatosCommand.java
@@ -4,23 +4,21 @@ import main.java.br.edu.ifpb.repository.ContatoRepository;
 import main.java.br.edu.ifpb.service.ContatoService;
 import main.java.br.edu.ifpb.domain.Contato;
 
+
 public class ListarContatosCommand implements Command {
     @Override
     public void execute() {
         ContatoService contatoService = new ContatoService(ContatoRepository.getInstance());
 
         System.out.println("\n\n=================================");
-        System.out.println("Listar contatos por Rede Social\n");
+        System.out.println("Listar contatos\n");
 
-        String atributoRedeSocial = "Instagram"; // Atributo de rede social desejado (por exemplo, "Instagram")
-
-        for (Contato c : contatoService.getContatos()) {
-            if (c.getRedeSocial() != null && c.getRedeSocial().equals(atributoRedeSocial)) {
-                System.out.println("Nome: " + c.getNome());
-                System.out.println("Rede Social: " + c.getRedeSocial());
-                // Adicione outros atributos relevantes aqui
-                System.out.println("====================");
-            }
+        for(Contato c: contatoService.getContatos()){
+            System.out.println(c);
         }
     }
 }
+
+
+
+
---
+++ src/main/java/br/edu/ifpb/commands/ListarContatosPorRedeCommand.java
@@ -0,0 +1,26 @@
+package main.java.br.edu.ifpb.commands;
+
+import main.java.br.edu.ifpb.repository.ContatoRepository;
+import main.java.br.edu.ifpb.service.ContatoService;
+import main.java.br.edu.ifpb.domain.Contato;
+
+public class ListarContatosPorRedeCommand implements Command {
+    @Override
+    public void execute() {
+        ContatoService contatoService = new ContatoService(ContatoRepository.getInstance());
+
+        System.out.println("\n\n=================================");
+        System.out.println("Listar contatos por Rede Social\n");
+
+        String atributoRedeSocial = "Instagram"; // Atributo de rede social desejado (por exemplo, "Instagram")
+
+        for (Contato c : contatoService.getContatos()) {
+            if (c.getRedeSocial() != null && c.getRedeSocial().equals(atributoRedeSocial)) {
+                System.out.println("Nome: " + c.getNome());
+                System.out.println("Rede Social: " + c.getRedeSocial());
+                // Adicione outros atributos relevantes aqui
+                System.out.println("====================");
+            }
+        }
+    }
+}
