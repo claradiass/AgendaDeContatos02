ec958bf0d1d9706a32c1e373a9027252cbfae052
Eduarda Souto <mariaeduardac604@gmail.com>
2023-11-08 10:34:30 -0300

    .

--- src/main/java/br/edu/ifpb/commands/AdicionarContatoCommand.java
+++ src/main/java/br/edu/ifpb/commands/AdicionarContatoCommand.java
@@ -6,11 +6,8 @@ import main.java.br.edu.ifpb.validators.AniversarioValidator;
 import main.java.br.edu.ifpb.validators.EmailValidator;
 import main.java.br.edu.ifpb.validators.IntervalValidator;
 import main.java.br.edu.ifpb.validators.NonEmptyValidator;
-<<<<<<< HEAD
 import main.java.br.edu.ifpb.validators.UserValidator;
-=======
 import main.java.br.edu.ifpb.validators.TelefoneValidator;
->>>>>>> 1dfffd9668bc393b4d09d33f438d0f90b15d1488
 import main.java.br.edu.ifpb.validators.ValidationContext;
 
 public class AdicionarContatoCommand implements Command {
